8d12973fb849577db56c3546b210d4744b8f2ec0
Renee Mei and Sam Kirsch <renee+sam@>
2017-04-18 13:28:47 -0700

    add include method to load team data

--- Controllers/DivisionController.cs
+++ Controllers/DivisionController.cs
@@ -24,7 +24,7 @@ namespace SportsOrganizer.Controllers
 
         public IActionResult Details(int id)
         {
-            var thisDivision = db.Divisions.FirstOrDefault(divisions => divisions.DivisionId == id);
+            var thisDivision = db.Divisions.Include(Divisions => Divisions.Teams).FirstOrDefault(divisions => divisions.DivisionId == id);
             return View(thisDivision);
         }
 
@@ -37,6 +37,34 @@ namespace SportsOrganizer.Controllers
         public IActionResult Create(Division division)
         {
             db.Divisions.Add(division);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisDivision = db.Divisions.FirstOrDefault(divisions => divisions.DivisionId == id);
+            return View(thisDivision);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Division division)
+        {
+            db.Divisions.Remove(division);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisDivision = db.Divisions.FirstOrDefault(divisions => divisions.DivisionId == id);
+            return View(thisDivision);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Division division)
+        {
+            db.Entry(division).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
